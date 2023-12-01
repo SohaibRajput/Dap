3345af13e6d091a959703129258a2c2a0ce574ac
Sohaib Rajput <srajput@payactiv.com.pk>
2023-11-30 23:20:13 -0800

    section 2

---
+++ API/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Cryptography.X509Certificates;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]  //api/user
+public class UserController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UserController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet]
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+
+        var users = _context.Users.ToList();
+        return users;
+    }
+
+    [HttpGet("{id}")] // api/users/2
+    public ActionResult<AppUser> GetUser(int id)
+    {
+        return _context.Users.Find(id);
+
+    }
+}
